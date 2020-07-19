3d3135f028ca644884ba0c6c5097145b4ecac94a
Cláudio Mindouro <claudiopedrom@gmail.com>
2020-07-19 17:41:18 +0100

    feat(fetch): swr fetch and mutate

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback } from 'react'
 import { Link } from 'react-router-dom'
+import { mutate as mutateGlobal } from 'swr'
+import { useFetch } from '../hooks/useFetch'
+import api from '../services/api'
 
 interface User {
   id: number
@@ -7,21 +10,38 @@ interface User {
 }
 
 const UserList: React.FC = () => {
-  const [data, setData] = useState<User[]>([])
+  const { data, mutate } = useFetch<User[]>('users')
 
-  useEffect(() => {
-    fetch('http://localhost:3333/users').then((response) => {
-      response.json().then((users) => {
-        setData(users)
+  const handleNameChange = useCallback(
+    (id: number) => {
+      api.put(`users/${id}`, { name: 'Bartolomeu' })
+
+      const updatedUsers = data?.map((user) => {
+        if (user.id === id) {
+          return { ...user, name: 'Bartolomeu' }
+        }
+
+        return user
       })
-    })
-  }, [])
+
+      mutate(updatedUsers, false)
+      mutateGlobal(`users/${id}`, { id, name: 'Bartolomeu' })
+    },
+    [data, mutate]
+  )
+
+  if (!data) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <ul>
       {data.map((user) => (
         <li key={user.id}>
           <Link to={`/users/${user.id}`}>{user.name}</Link>
+          <button type="button" onClick={() => handleNameChange(user.id)}>
+            Alterar nome
+          </button>
         </li>
       ))}
     </ul>
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import UserList from './pages/UserList'
 import UserDetails from './pages/UserDetails'
 
+const ManyUserLists = () => (
+  <>
+    <UserList />
+    <UserList />
+    <UserList />
+  </>
+)
+
 export default function AppRoutes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={UserList} />
+        <Route exact path="/" component={ManyUserLists} />
         <Route path="/users/:id" component={UserDetails} />
       </Switch>
     </Router>
